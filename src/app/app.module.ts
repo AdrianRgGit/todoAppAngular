70f07555f1ee364caa1efe9f7f90c2335e246e6e
Adrián Ramírez <adrianramirezgalera@gmail.com>
2024-03-20 12:00:38 +0100

    creada plantilla

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TrashIconComponent } from './Components/Icons/trash-icon/trash-icon.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TrashIconComponent
   ],
   imports: [
     BrowserModule,
---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,ts}"],
+  theme: {
+    extend: {
+      colors: {
+        "purple": "#AF7EEB",
+      }
+    },
+  },
+  plugins: [],
+};
