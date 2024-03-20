469ede71b19b785c3fa3074f46ac2c059796983f
Adrián Ramírez <adrianramirezgalera@gmail.com>
2024-03-20 12:20:21 +0100

    layout

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,18 +3,13 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { TrashIconComponent } from './Components/Icons/trash-icon/trash-icon.component';
+import { TrashIconComponent } from './components/Icons/trash-icon/trash-icon.component';
+import { TaskComponent } from './components/task/task.component';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    TrashIconComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  declarations: [AppComponent, TrashIconComponent, TaskComponent],
+  imports: [BrowserModule, AppRoutingModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
